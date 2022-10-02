234ed0b20e17df00b161512471cd841d737004ae
Fady <Fadyasifein@gmail.com>
2022-10-01 21:15:44 -0400

    first commit

---
+++ unit-1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // 5 objects
+        VendingMachine doritos = new VendingMachine("Doritos", 3.50, 12); 
+        VendingMachine lays  = new VendingMachine("Lays", 3, 10);
+        VendingMachine pepsi = new VendingMachine("Pepsi", 4, 8); 
+        VendingMachine cocaCola = new VendingMachine("CocaCola", 4, 9); 
+        VendingMachine water = new VendingMachine("Water", 7, 15);
+    }
+}
--- unit-1/VendingMachine.java
+++ unit-1/VendingMachine.java
@@ -1,31 +1,40 @@
 public class VendingMachine {
     
+    // Instance variables 
     private String productName; 
     private double price; 
     private static final int MAX_QUANTITY = 50; 
     private int currentStock;
 
+    // no-arg constructor (default product)
     public VendingMachine() {
-
+        this("Granola-bar", 2, 12);
     }
 
+    // (String, double, int) constructor set's arguments equal to instance variables
     public VendingMachine(String productName, double price, int currentStock) {
-        
+        this.productName = productName; 
+        this.price = price; 
+        this.currentStock = currentStock; 
     }
 
+    // returns price
     public double getPrice() {
         return price; 
     }
 
+    // sets argument passed in equal to price Instance variable
     public void setPrice(double price) {
         this.price = price;
     }
 
+    // returns currentStock
     public int getCurrentStock() {
         return currentStock; 
     }
 
+    // returns the total sales
     public double checkTotalSales() {
-        return price; // TODO: Idk what to do here
+        return price * (50 - currentStock); 
     }
 }
